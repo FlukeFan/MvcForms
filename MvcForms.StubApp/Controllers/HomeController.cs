82370cd491dcb2cad22faac8f34b31732b411625
Richard Brown <flukefan@googlemail.com>
2016-11-16 10:03:10 +0000

    Added MVC to StubApp

---
+++ MvcForms.StubApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MvcForms.StubApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello world");
+        }
+    }
+}
---
+++ MvcForms.StubApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcForms.StubApp.Global" Language="C#" %>
