017f0362c4699d580377e56aa0fa7fccbc47bcc0
Richard Brown <flukefan@googlemail.com>
2017-07-30 12:45:33 +0100

    Removed dependency on Json parser (added by HtmlTags)

---
+++ MvcForms.Tests/DesignContraintsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace MvcForms.Tests
+{
+    [TestFixture]
+    public class DesignContraintsTests
+    {
+        [Test]
+        public void DependenciesHaveNotChanged()
+        {
+            var folder = @"..\..\..\_output";
+            var name = "MvcForms";
+
+            NugetDependency.VerifyDependencies(folder, name, new string[]
+            {
+                "jQuery:1.9.0",
+                "HtmlTags:*",
+            });
+        }
+    }
+}
