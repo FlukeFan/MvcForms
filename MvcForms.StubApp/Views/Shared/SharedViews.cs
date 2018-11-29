f7a9af383c2256e29b25ffba31fe616c703f94c4
Richard Brown <fluke_fan@hotmail.com>
2018-11-29 15:30:03 +0000

    Added remaining js files.

--- MvcForms.StubApp/Views/Shared/SharedViews.cs
+++ MvcForms.StubApp/Views/Shared/SharedViews.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Text;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Html;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.AspNetCore.Mvc.TagHelpers.Internal;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.DependencyInjection;
 using MvcForms.StubApp.Utility;
 
 namespace MvcForms.StubApp.Views.Shared
@@ -35,15 +30,6 @@ namespace MvcForms.StubApp.Views.Shared
 
             selector.Append("</select>");
             return new HtmlString(selector.ToString());
-        }
-
-        // from:  https://stackoverflow.com/a/45088852/357728
-        public static string AddFileVersionToPath(this HttpContext context, string path)
-        {
-            IMemoryCache cache = context.RequestServices.GetRequiredService<IMemoryCache>();
-            var hostingEnvironment = context.RequestServices.GetRequiredService<IHostingEnvironment>();
-            var versionProvider = new FileVersionProvider(hostingEnvironment.WebRootFileProvider, cache, context.Request.Path);
-            return versionProvider.AddFileVersionToPath(path);
         }
     }
 }
